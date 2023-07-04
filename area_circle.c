ebddd6b3dd112c9ab9bb91862f74c800a5cc3241
Rajiblochan Pramanik <138567893+RajibPramanik@users.noreply.github.com>
2023-07-04 13:36:23 +0530

    Add files via upload

---
+++ area_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    //write a c code for area of a circle?
+    float radius,area,pi=3.14;
+    radius=5;
+    area=pi*radius*radius;
+    printf("The area of circle is %f",area);
+    return 0;
+}
